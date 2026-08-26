e93d714b9db61945acefc678f7625a1862d88612
Arkadii <qatiktak@gmail.com>
2026-08-26 09:51:03 +0200

    Add6L27

---
+++ src/main/java/com/demoqa/pages/elements/ButtonPage.java
@@ -0,0 +1,12 @@
+package com.demoqa.pages.elements;
+
+import com.demoqa.core.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class ButtonPage extends BasePage {
+    public ButtonPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
